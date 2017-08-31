1ded3da30e063c356c6edf616603c9caa00a8b7a
Carlos Willians <demolidorjava@gmail.com>
2017-08-31 16:48:54 -0300

    Exemplos de gráficos completo

---
+++ exemplosgrafico/src/main/java/br/com/javaparaweb/capitulo12/JFreeChart/CidadeColunasBean.java
@@ -0,0 +1,44 @@
+package br.com.javaparaweb.capitulo12.JFreeChart;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.logging.Logger;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+public class CidadeColunasBean {
+	private StreamedContent grafico;
+	private static final Logger log = Logger.getLogger(CidadeColunasBean.class.getName());
+
+public CidadeColunasBean() {
+	try {
+		JFreeChart graficoColunas = ChartFactory.createBarChart("Cinco cidades mais populosas de Santa Catarina", "cidades", "população", this.geraDados(), PlotOrientation.VERTICAL,false, true, false);
+		File arquivoGrafico = new File("colunas.png");
+		ChartUtilities.saveChartAsPNG(arquivoGrafico, graficoColunas, 500, 325);	
+		this.grafico = new DefaultStreamedContent(new FileInputStream(arquivoGrafico), "image/png");
+	}catch(Exception e) {
+		log.severe(e.getMessage());
+	}
+}
+
+	private DefaultCategoryDataset geraDados() {
+		DefaultCategoryDataset dts = new DefaultCategoryDataset();
+		dts.setValue(new Double(334002.0), "População", "Blumenau");
+		dts.setValue(new Double(204667.0), "População", "Criciúma");
+		dts.setValue(new Double(461524.0), "População", "Florianópolis");
+		dts.setValue(new Double(554601.0), "População", "Joinville");
+		dts.setValue(new Double(228561.0), "População", "São José");
+		
+		return dts;
+	}
+	
+	public StreamedContent getGrafico() {
+		return this.grafico;
+	}
+}
